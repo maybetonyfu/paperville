4a53dbc8009931ba00cd56babd7d577e220a70f5
Tony Fu <tonyf@gorapid.com.au>
2016-04-19 18:33:09 +1000

    Change assets

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -7,63 +7,63 @@ const Menu = Radium(() => {
 
 
     let menuStyle = {
-        
+
         width: "100%",
-        
+
         height: "100%",
-        
+
         textAlign: "center"
-        
+
     }
-    
+
     let titleStyle = {
-        
+
         marginTop: "30%",
-        
+
         fontSize: "4vh",
-        
+
         textAlign: "center",
-        
+
         fontFamily: "'Sue Ellen Francisco', cursive",
-        
+
         marginBottom: "10%"
-        
+
     }
 
     let menuLinkStyle = {
-        
+
         textDecoration: "none",
-        
+
         textAlign: "center",
-        
+
         display: "block",
-        
+
         width: "15vh",
-        
+
         height: "4vh",
-        
+
         lineHeight: "4vh",
-        
+
         color: "black",
-        
+
         marginLeft: "50%",
-        
+
         transform: "translateX(-50%)",
-        
+
         borderRadius: "10px",
-        
+
         border: "2px solid black",
-        
+
         marginTop : "2vh",
-        
+
         backgroundColor: "white",
 
     }
-    
+
     return (
 
         <div style={menuStyle}>
-            <p style={titleStyle}>Paper Bakery</p>
+            <p style={titleStyle}>Paper Farm</p>
             <Link style={menuLinkStyle} to="/campaign">Campaign</Link>
             <Link style={menuLinkStyle} to="/setting">Setting</Link>
         </div>
@@ -73,7 +73,7 @@ const Menu = Radium(() => {
 )
 
 Menu.PropTypes = {
-    
+
 }
 
 
--- src/reducers/levels.js
+++ src/reducers/levels.js
@@ -33,10 +33,10 @@ let initLevels = {
             gravity: "DOWN"
         },
         groupMap: {
-            0: "flour",
-            1: "sugar",
-            2: "milk",
-            3: "egg"
+            0: "seeds",
+            1: "scythe",
+            2: "fields",
+            3: "sprouts"
         },
         maxMove: 10,
         objectiveAmount: [
@@ -62,164 +62,164 @@ let initLevels = {
             }
         ]
     },
-    2: {
-        id: 2,
-        name: "First order",
-        description: [
-            "Welcome on board! ",
-            "Mr Thompkinson likes to have his croisant 7:00 am every morning. Better prepare a new batch he comes",
-            "Recipe: ",
-            "flour  X 10",
-            "sugar  X 10",
-            "milk   X 10",
-            "egg    X 10",
-            "salt   X 5",
-            "(The hourglass on top-left corner indicates how many move you have left. Use them carefully)",
-        ],
-        winMessage: {
-            title: "Good job!",
-            symbol: "croisant",
-            message: "You made a lovely croisant"
-        },
-        lostMessage: {
-            title: "Darn!",
-            message: ""
-        },
-        boardConfig: {
-            rows: 6,
-            cols: 6,
-            groups: {
-                0: 3,
-                1: 3,
-                2: 3,
-                3: 3,
-                4: 1,
-            },
-            gravity: "DOWN"
-        },
-        groupMap: {
-            0: "flour",
-            1: "sugar",
-            2: "milk",
-            3: "egg",
-            4: "salt"
-        },
-        maxMove: 15,
-        objectiveAmount: [
-            {
-                group: 0,
-                minimum: 10,
-                description: "Collect at least 10 flour"
-            },
-            {
-                group: 1,
-                minimum: 10,
-                description: "Collect at least 10 roller"
-            },
-            {
-                group: 2,
-                minimum: 10,
-                description: "Collect at least 10 milk"
-            },
-            {
-                group: 3,
-                minimum: 10,
-                description: "Collect at least 10 egg"
-            },
-            {
-                group: 4,
-                minimum: 5,
-                description: "Collect at least 5 salt"
-            }
-        ]
-    },
-    3: {
-        id: 3,
-        name: "Easy as pie",
-        description: [
-            "Nice work! I hope you make good pie though, cause I can really have some now",
-            "Make an mushroom pie",
-            "Recipe: ",
-            "flour  X 10",
-            "milk X 10",
-            "egg    X 10",
-            "mushroom  X 3",
-            "* sugar  < 10",
-            "(Don't put too much sugar. You will lose if collect 11 or more sugar)",
-        ],
-        winMessage: {
-            title: "Well done!",
-            symbol: "pie",
-            message: "Best pie ever!"
-        },
-        lostMessage: {
-            title: "Darn!",
-            message: ""
-        },
-        boardConfig: {
-            rows: 6,
-            cols: 6,
-            groups: {
-                0: 3,
-                1: 3,
-                2: 3,
-                3: 3,
-                4: 1,
-            },
-            gravity: "DOWN"
-        },
-        groupMap: {
-            0: "sugar",
-            1: "flour",
-            2: "milk",
-            3: "egg",
-            4: "mushroom"
-        },
-        maxMove: 20,
-        objectiveAmount: [
-            {
-                group: 0,
-                maximum: 11,
-                description: "Collect at most 11 sugar"
-            },
-            {
-                group: 1,
-                minimum: 10,
-                description: "Collect at least 10 flour"
-            },
-            {
-                group: 2,
-                minimum: 10,
-                description: "Collect at least 10 butter"
-            },
-            {
-                group: 3,
-                minimum: 10,
-                description: "Collect at least 10 egg"
-            },
-            {
-                group: 4,
-                minimum: 5,
-                description: "Collect at least 5 mushroom"
-            }
-        ]
-    },
+//   2: {
+//       id: 2,
+//       name: "First order",
+//       description: [
+//           "Welcome on board! ",
+//           "Mr Thompkinson likes to have his croisant 7:00 am every morning. Better prepare a new batch he comes",
+//           "Recipe: ",
+//           "flour  X 10",
+//           "sugar  X 10",
+//           "milk   X 10",
+//           "egg    X 10",
+//           "salt   X 5",
+//           "(The hourglass on top-left corner indicates how many move you have left. Use them carefully)",
+//       ],
+//       winMessage: {
+//           title: "Good job!",
+//           symbol: "croisant",
+//           message: "You made a lovely croisant"
+//       },
+//       lostMessage: {
+//           title: "Darn!",
+//           message: ""
+//       },
+//       boardConfig: {
+//           rows: 6,
+//           cols: 6,
+//           groups: {
+//               0: 3,
+//               1: 3,
+//               2: 3,
+//               3: 3,
+//               4: 1,
+//           },
+//           gravity: "DOWN"
+//       },
+//       groupMap: {
+//           0: "flour",
+//           1: "sugar",
+//           2: "milk",
+//           3: "egg",
+//           4: "salt"
+//       },
+//       maxMove: 15,
+//       objectiveAmount: [
+//           {
+//               group: 0,
+//               minimum: 10,
+//               description: "Collect at least 10 flour"
+//           },
+//           {
+//               group: 1,
+//               minimum: 10,
+//               description: "Collect at least 10 roller"
+//           },
+//           {
+//               group: 2,
+//               minimum: 10,
+//               description: "Collect at least 10 milk"
+//           },
+//           {
+//               group: 3,
+//               minimum: 10,
+//               description: "Collect at least 10 egg"
+//           },
+//           {
+//               group: 4,
+//               minimum: 5,
+//               description: "Collect at least 5 salt"
+//           }
+//       ]
+//   },
+//   3: {
+//       id: 3,
+//       name: "Easy as pie",
+//       description: [
+//           "Nice work! I hope you make good pie though, cause I can really have some now",
+//           "Make an mushroom pie",
+//           "Recipe: ",
+//           "flour  X 10",
+//           "milk X 10",
+//           "egg    X 10",
+//           "mushroom  X 3",
+//           "* sugar  < 10",
+//           "(Don't put too much sugar. You will lose if collect 11 or more sugar)",
+//       ],
+//       winMessage: {
+//           title: "Well done!",
+//           symbol: "pie",
+//           message: "Best pie ever!"
+//       },
+//       lostMessage: {
+//           title: "Darn!",
+//           message: ""
+//       },
+//       boardConfig: {
+//           rows: 6,
+//           cols: 6,
+//           groups: {
+//               0: 3,
+//               1: 3,
+//               2: 3,
+//               3: 3,
+//               4: 1,
+//           },
+//           gravity: "DOWN"
+//       },
+//       groupMap: {
+//           0: "sugar",
+//           1: "flour",
+//           2: "milk",
+//           3: "egg",
+//           4: "mushroom"
+//       },
+//       maxMove: 20,
+//       objectiveAmount: [
+//           {
+//               group: 0,
+//               maximum: 11,
+//               description: "Collect at most 11 sugar"
+//           },
+//           {
+//               group: 1,
+//               minimum: 10,
+//               description: "Collect at least 10 flour"
+//           },
+//           {
+//               group: 2,
+//               minimum: 10,
+//               description: "Collect at least 10 butter"
+//           },
+//           {
+//               group: 3,
+//               minimum: 10,
+//               description: "Collect at least 10 egg"
+//           },
+//           {
+//               group: 4,
+//               minimum: 5,
+//               description: "Collect at least 5 mushroom"
+//           }
+//       ]
+//   },
 }
 
 const levels = (state = initLevels, action) => {
-    
+
     switch (action.type) {
-        
+
     case "SOME_ACTION":
-        
+
         return state
-        
+
     default:
-    
+
         return state
-        
+
     }
-    
+
 }
 
 export default levels
